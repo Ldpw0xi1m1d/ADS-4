375ff0a4485554398b77fcb6030c6a792fdd7895
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-12 17:52:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,34 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+
+	int count = 0;
+
+	for (int i = 0; i < len; i++) {
+		for (int j = i; j < len; j++) {
+			if (arr[i] + arr[j] == value) {
+				count++;
+			}
+		}
+	}
+	return count;
+	return 0;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+
+int countPairs2(int* arr, int len, int value) {
+
+	int count = 0;
+
+	for (int i = 0; i < len; i++) {
+		for (int j = len; j >= i; j--) {
+			if (arr[i] + arr[j] == value) {
+				count++;
+			}
+		}
+	}
+	return count;
+	return 0;
 }
+
 int countPairs3(int *arr, int len, int value) {
   return 0;
 }
