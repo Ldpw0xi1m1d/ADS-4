3c73bc0ae09fd23de917c5b478a88261c489d66e
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-16 19:55:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,64 +3,64 @@ int countPairs1(int* arr, int len, int value) {
 int count = 0;
 
 for (int i = 0; i < len; i++) {
- for (int j = i+1; j < len; j++) {
- if (arr[i] + arr[j] == value) {
- count++;
-  }
- }
+for (int j = i+1; j < len; j++) {
+if (arr[i] + arr[j] == value) {
+count++;
+}
+}
 }
 return count;
- return 0;
+return 0;
 }
 
 int countPairs2(int* arr, int len, int value) {
- int count = 0;
+int count = 0;
 
 for (int i = 0; i < len; i++) {
- for (int j = len; j > i; j--) {
- if (arr[j] > value) continue;
-  if (arr[i] + arr[j] == value) {
-  count++;
-  }
- }
+for (int j = len; j > i; j--) {
+if (arr[j] > value) continue;
+if (arr[i] + arr[j] == value) {
+count++;
+}
+}
 }
 return count;
- return 0;
+return 0;
 }
 
 int countPairs3(int* arr, int len, int value) {
-  int count = 0;
-  int right = len-1;
-  int left = 0;
-  int index = 0;
-  int mid;
+int count = 0;
+int right = len-1;
+int left = 0;
+int index = 0;
+int mid;
 
 for (int i = 0; i < len-1; i++) {
- int prim = 0; 
-  left = i + 1;
-  mid = (left + right) / 2;
-  while (left <= right) {
- if (arr[mid] + arr[i] == value) {
- count++;
- prim = 1;
+int prim = 0; 
+left = i + 1;
+mid = (left + right) / 2;
+while (left <= right) {
+if (arr[mid] + arr[i] == value) {
+count++;
+prim = 1;
 break;
 }
- if (arr[mid]+arr[i] > value) right = mid - 1;
- else left = mid + 1;
- mid = (left + right) / 2;
- }
- if (prim) {
-  int middle = mid;
-  index = mid;
-  while (arr[index + 1] + arr[i] == value) {
-   count++;
-   index++;
- }
-while (arr[middle - 1] + arr[i] == value && middle - 1 != i) {
- count++;
- middle--;
-   }
-  }
+if (arr[mid]+arr[i] > value) right = mid - 1;
+else left = mid + 1;
+mid = (left + right) / 2;
 }
- return count;
- }
+if (prim) {
+int middle = mid;
+index = mid;
+while (arr[index + 1] + arr[i] == value) {
+count++;
+index++;
+}
+while (arr[middle - 1] + arr[i] == value && middle - 1 != i) {
+count++;
+middle--;
+}
+}
+}
+return count;
+}
