33548cd31b3fa3195f699d3b5b539fc04ca4d35e
Ldpw0xi1m1d <99180755+Ldpw0xi1m1d@users.noreply.github.com>
2022-03-21 23:00:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int* arr, int len, int value) {
 int count = 0;
 
-for (int i = 0; i < len; i++) {
+for (int i = 0; i < len-1; i++) {
 for (int j = i + 1; j < len; j++) {
 if (arr[i] + arr[j] == value) {
 count++;
@@ -16,8 +16,8 @@ return 0;
 int countPairs2(int* arr, int len, int value) {
 int count = 0;
 
-for (int i = 0; i < len; i++) {
-for (int j = len; j > i; j--) {
+for (int i = 0; i < len-1; i++) {
+for (int j = len-1; j > i; j--) {
 if (arr[j] > value) continue;
 if (arr[i] + arr[j] == value) {
 count++;
